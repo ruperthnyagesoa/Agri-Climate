8f1f8eeb0e51085b0c416d0bfd68423d90edd4d3
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2024-05-08 12:24:25 +0300

    ft:updates

---
+++ backend/weather.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+
+def get_temperature_forecast(forecast_days, latitude, longitude):
+    api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation_probability&forecast_days={forecast_days}"
