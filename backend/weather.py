1b226b73b6211d7ad47d8c119d754f5266ae9bd1
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2024-05-08 12:25:33 +0300

    ft:request API

--- backend/weather.py
+++ backend/weather.py
@@ -4,3 +4,5 @@ import json
 
 def get_temperature_forecast(forecast_days, latitude, longitude):
     api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation_probability&forecast_days={forecast_days}"
+# Send the API request
+    response = requests.get(api_url)
